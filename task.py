ff88201f22f27aa7af8553135530e083457bd31e
mike lynch <103330150+infosecmikey@users.noreply.github.com>
2026-07-11 22:57:59 -0400

    Day1: Python band-name generator Project
    Upload task.py to repo main

---
+++ task.py
@@ -0,0 +1,17 @@
+import time
+
+print("Hello, Welcome to Band Together. Find out what your band's name will be!")
+time.sleep(1)
+userCity = input("What city did you grow up in?\n")
+time.sleep(2)
+print("Nice! I always wanted to visit " + userCity + "!")
+time.sleep(2)
+userPetName = input("What is your pet's name?\n")
+time.sleep(2)
+print(userPetName + ", is the best name ever. I wish I would've\nthought of that name for my dog!")
+
+time.sleep(5)
+print("All right, eureka, I've got it!")
+time.sleep(5)
+print(userCity + userPetName + "! Rock on my friend!")
+time.sleep(15)
